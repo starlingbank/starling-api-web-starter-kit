dd38b7278fb952093c11c8d829b1e4a86e0994f7
Daniel Osborne <dan.osborne@starlingbank.com>
2017-04-04 16:45:26 +0100

    Fix merge

--- src/routes/PersonalAccess/views/PersonalAccessView.js
+++ src/routes/PersonalAccess/views/PersonalAccessView.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import URLSearchParams from 'url-search-params'
-import {Loader, Message, Statistic, Grid, Container, Segment, Header, Image, Icon, Label} from 'semantic-ui-react'
-import Dashboard from '../../../components/Dashboard/Dashboard'
-import UserDenied from "../../../components/UserDenied/UserDenied";
+import React from "react";
+import URLSearchParams from "url-search-params";
+import {Loader, Message, Button, Statistic, Grid, Container, Segment, Header, Image, Icon, Label} from 'semantic-ui-react'
+import Dashboard from "../../../components/Dashboard/Dashboard";
+import UserDenied from "../../../components/UserDenied/UserDenied"
 import {Link} from 'react-router'
 import './PersonalAccessView.scss'
 
